69b32457eab60d7deda59397655c899328d565dd
Aljosha Novakovic <ollynov@gmail.com>
2016-06-22 23:12:23 -0700

    Update w3d3.js

--- w3d3.js
+++ w3d3.js
@@ -80,11 +80,89 @@ function countNestedKeys(obj) {
 	return newObj;
 }
 
+// FILTER BASIC REQUIREMENTS
+
+function filter(coll, predicate) {
+  var acc = [];
+  var obj = {};
+  if (Array.isArray(coll) {
+	  each(coll, function(element, i) {
+	    if (predicate(element, i)) {
+	      acc.push(element);
+	    }
+	  });
+	  return acc;
+   } else {
+   	  each(obj, function(value, key) {
+   	  	if (predicate(value, key) {
+   	  		obj[key] = value;
+   	  	})
+   	  }
+   	  return obj;
+   }
+
+
+}
 
 
 
+// 1. Returns only even numbers
+function evens(arr) {
+	return filter(arr, function(element) {
+		 return element % 2 === 0
+	})
+}
 
-// Operate 
+//2. multiplesOfThree Write a function called multiplesOfThree that takes an array of numbers as a 
+// parameter, and returns an array of only the numbers that are multiples of three
+
+function multiplesOfThree(numbersArr) {
+	return filter(numbersArr, function(num) {
+		return element % 3 === 0;
+	})
+}
+
+function positives(nums) {
+	return filter(nums, function(num) {
+		return num > 0;
+	})
+}
+
+function evenLength(strings) {
+	return filter(strings, function(string) {
+		return string.length % 2 === 0
+	})
+}
+
+function odds(nums) {
+	return filter(nums, function(num) {
+		return num % 2 !== 0;
+	})
+}
+
+function negatives(nums) {
+	return filter(nums, function(num) {
+		return num < 0
+	})
+}
+
+function largerThanSix(nums) {
+	return filter(nums, function(num) {
+		return num > 6
+	})
+}
+
+function startsWithChar(strings, char) {
+	return filter(strings, function(string) {
+		return string[0] === char;
+	})
+}
+
+function evenIndexedEvenLengths(strings) {
+	return filter(strings, function(string, i) {
+		return (string.length % 2 === 0 && i % 2 === 0)
+	})
+}
 
 
 
